e62644cd82f46327c26c822db075ca611c445c40
yonnie <yonathanmichael63@gmail.com>
2021-07-17 10:25:50 -0400

    Now able to switch tabs,update tabs, switch windows. All events fired succesfully.

--- background.js
+++ background.js
@@ -10,6 +10,16 @@ chrome.tabs.onActivated.addListener(function () {
 	}, 500);
 });
 
+chrome.windows.onFocusChanged.addListener(function () {
+	setTimeout(() => {
+		getCurrentTab();
+	}, 500);
+});
+
+chrome.windows.onRemoved.addListener(function () {
+	console.log("Window Closed");
+});
+
 async function getCurrentTab() {
 	let queryOptions = { active: true, currentWindow: true };
 	let [tab] = await chrome.tabs.query(queryOptions);
